91a6c77e3a3d0df36832650fb7e2e9d910bfc023
Caroline Gilbert <carolinecgilbert6@gmail.com>
2023-02-27 17:06:05 +0100

    final

--- HW3_source/CFiles/OwnCode/Controller.c
+++ HW3_source/CFiles/OwnCode/Controller.c
@@ -1,16 +1,17 @@
 // Initialize which task to complete: 
 // task = 1: Task 6
-// task = 2: Task 8 or Task 9
-task = 1;
-error_xk = xg - x0;
-error_yk = yg - y0;
+// task = 2: Task 8
+// task = 3: Task 9
+task = 3;
+error_xg = xg - x;
+error_yg = yg - y;
 //
 // TASK 6
 //
 if (task == 1)
 {
-    // Calculate error when v=0
-    thetaR = atan2(error_yk,error_xk)*(180/pi);
+    // Calculate w when v=0
+    thetaR = atan2(error_yg,error_xg)*(180/pi);
     if ((thetaR == -180 || thetaR == 180) && (theta == 180 || theta == -179 || theta == 179)) 
     {
         w = 0;
@@ -25,18 +26,39 @@ if (task == 1)
 }
 
 // 
-// Task 8 or Task 9
+// Task 8
 //
 else if (task == 2) 
 {
-    // Calculate error when w=0
-    thetaR = atan2(error_yk,error_xk)*(180/pi);
-    error_theta = thetaR - theta;
+    // Calculate v when w=0
     w = 0;
-    error_x0 = x0 - x;
-	error_y0 = y0 - y;
-	error_v0 = (cos(theta*pi/180)*error_x0) + (sin(theta*pi/180)*error_y0);
-	v = Kpsi_task8_9 * error_v0;
+    error_x0 = (x0 - x);
+	error_y0 = (y0 - y);
+	vc = (cos(theta*pi/180)*error_x0) + (sin(theta*pi/180)*error_y0);
+	v = (Kpsi_task8_9 * vc) / 100;
+}
+
+//
+// Task 9
+//
+else if (task == 3) 
+{
+    // Calculate w
+    thetaR = atan2(error_yg/100,error_xg/100)*(180/pi);
+    if ((thetaR == -180 || thetaR == 180) && (theta == 180 || theta == -179 || theta == 179)) 
+    {
+        w = 0;
+    }
+    else 
+    {
+        error_theta = thetaR - theta;
+        w = Kpsi_task6 * error_theta;
+    }
+    // Calculate v
+    error_x0 = (x0 - x);
+	error_y0 = (y0 - y);
+	vc = (cos(theta*pi/180)*error_x0) + (sin(theta*pi/180)*error_y0);
+	v = (Kpsi_task8_9 * vc) / 100;
 }
 
 // Update right and left
--- HW3_source/CFiles/OwnCode/OwnVariables.c
+++ HW3_source/CFiles/OwnCode/OwnVariables.c
@@ -3,19 +3,20 @@ const double h = 1;
 double R_true = 0.1001405119340;
 double L_true = 0.5052864456892;
 // Max Kpsi values from task 5 and task 7 calculations divided by error to minimize oscillation
-double Kpsi_min_error = 2;
-double Kpsi_task6 = 5; //(2*L_true)/(R_true*h*Kpsi_min_error);
-double Kpsi_task8_9 = 2/(R_true*h*Kpsi_min_error);
+float Kpsi_error_t6 = 2;
+float Kpsi_error_t8 = 2;
+float Kpsi_task6 = (2*L_true)/(R_true*h*Kpsi_error_t6);
+float Kpsi_task8_9 = 2/(R_true*h*Kpsi_error_t8);
 float w = 0;
 float v = 0;
 float thetaR = 0;
 float pi = 3.14159265359;
 // Controller error
 float error_theta = 0;
-float error_v0 = 0;
+float vc = 0;
+float error_xg = 0;
+float error_yg = 0;
 float error_x0 = 0;
 float error_y0 = 0;
-float error_xk = 0;
-float error_yk = 0;
 // Task variable
 int task = 0;
